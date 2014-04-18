acb9ec08ba0a82a366713819dc651eec716ea8ec
zubiar-arbi <zubair.afzal@arbisoft.com>
2014-04-18 13:15:52 +0500

    move upload code for assets to AssetsView STUD-1523

--- cms/djangoapps/contentstore/features/upload.py
+++ cms/djangoapps/contentstore/features/upload.py
@@ -25,8 +25,11 @@ def go_to_uploads(_step):
 
 
 @step(u'I upload the( test)? file "([^"]*)"$')
-def upload_file(_step, is_test_file, file_name):
-    world.click_link('Upload New File')
+def upload_file(_step, is_test_file, file_name, button_text=None):
+    if button_text:
+        world.click_link(button_text)
+    else:
+        world.click_link('Upload New File')
 
     if not is_test_file:
         _write_test_file(file_name, "test file")
@@ -38,6 +41,9 @@ def upload_file(_step, is_test_file, file_name):
     close_css = 'a.close-button'
     world.css_click(close_css)
 
+@step(u'I upload the( test)? file "([^"]*)" by clicking "([^"]*)"')
+def upload_file_on_button_press(_step, is_test_file, file_name, button_text=None):
+    upload_file(_step, is_test_file, file_name, button_text)
 
 @step(u'I upload the files "([^"]*)"$')
 def upload_files(_step, files_string):
